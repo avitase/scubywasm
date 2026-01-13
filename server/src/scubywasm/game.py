322607d9c3a4a3c02bcd0dc69ad64933f21244cc
Nis Meinert <nis.meinert@dlr.de>
2026-01-13 15:13:28 +0100

    Improve game.py

--- server/src/scubywasm/game.py
+++ server/src/scubywasm/game.py
@@ -46,7 +46,11 @@ class Game:
             rng = random.Random(seed)
             phi = 1.61803398875  # golden ratio
             init_poses = [
-                Pose(x=(i / phi) % 1.0, y=i / (n * m), heading=360 * rng.random())
+                Pose(
+                    x=(i / phi + 0.5) % 1.0,
+                    y=(i / (n * m) + 0.5) % 1.0,
+                    heading=360 * rng.random(),
+                )
                 for i in range(n * m)
             ]
             rng.shuffle(init_poses)
@@ -55,39 +59,53 @@ class Game:
         batched_agent_ids = [agent_ids[i * m : (i + 1) * m] for i in range(n)]
 
         self._teams = [(agent, ids) for agent, ids in zip(agents, batched_agent_ids)]
+        self._log = [
+            {
+                "ships": {
+                    agent_id: dict(x=[], y=[], heading=[], alive=[])
+                    for agent_id in batch
+                },
+                "shots": {
+                    agent_id: dict(x=[], y=[], lifetime=[]) for agent_id in batch
+                },
+                "scores": {agent_id: [] for agent_id in batch},
+            }
+            for batch in batched_agent_ids
+        ]
 
     @property
     def config(self):
         return self._engine.config
 
-    def tick(self, n_times=1):
-        ships = dict()
-        shots = dict()
-        scores = dict()
+    @property
+    def log(self):
+        return self._log
 
-        for agent, agent_ids in self._teams:
+    def tick(self, n_times=1):
+        team_alive = [False] * len(self._teams)
+        for i, (agent, agent_ids) in enumerate(self._teams):
             agent.clear_world_state()
 
             for agent_id in agent_ids:
                 is_alive = self._engine.is_alive(agent_id)
+
                 ship_pose = self._engine.get_ship_pose(agent_id)
-                if is_alive:
-                    ships[agent_id] = {
-                        "x": round(ship_pose.x, 4),
-                        "y": round(ship_pose.y, 4),
-                        "heading": round(ship_pose.heading, 1),
-                    }
+                team_alive[i] = True
+
+                ship = self._log[i]["ships"][agent_id]
+                ship["x"].append(round(ship_pose.x, 4))
+                ship["y"].append(round(ship_pose.y, 4))
+                ship["heading"].append(round(ship_pose.heading, 1))
+                ship["alive"].append(is_alive)
 
                 shot_pose, lifetime = self._engine.get_shot_pose(agent_id)
-                if lifetime > 0:
-                    shots[agent_id] = {
-                        "x": round(shot_pose.x, 4),
-                        "y": round(shot_pose.y, 4),
-                        "heading": round(shot_pose.heading, 1),
-                    }
+                shot = self._log[i]["shots"][agent_id]
+                shot["x"].append(round(shot_pose.x, 4))
+                shot["y"].append(round(shot_pose.y, 4))
+                shot["lifetime"].append(lifetime)
 
                 score = self._engine.get_score(agent_id)
-                scores[agent_id] = score
+                self._log[i]["scores"][agent_id].append(score)
 
                 for other, _ in self._teams:
                     other.update_ship(agent_id, is_alive=is_alive, pose=ship_pose)
@@ -99,7 +117,7 @@ class Game:
                 action = agent.make_action(agent_id, self.ticks)
                 self._engine.set_action(agent_id, action)
 
-        n_alive = self._engine.tick(n_times)
+        self._engine.tick(n_times)
         self.ticks += n_times
 
-        return n_alive, {"ships": ships, "shots": shots, "scores": scores}
+        return sum(team_alive)
