4403176d654caa3b0f634a5b660113fb687e2c01
Nis Meinert <mail@nismeinert.de>
2026-01-25 14:56:29 +0100

    agents: make Lua agent slightly more interesting
    The Lua agent now behaves slightly more interesting and is now part of the
    release artifacts.

--- agents/lua_agent.lua
+++ agents/lua_agent.lua
@@ -5,8 +5,49 @@ function update_ship(agent_id, hp, x, y, heading) end
 function update_shot(agent_id, lifetime, x, y, heading) end
 function update_score(agent_id, score) end
 
+local is_moving = {}
+local rng_state = {}
+
+local function next_rnd(state)
+    state = (1664525 * state + 1013904223) & 0xffffffff
+
+    -- drop sign bit... u is now in [0, 2^31]
+    local u = state & 0x7fffffff
+
+    return state, u / (2^31)
+end
+
 function make_action(agent_id, tick)
-    local block = math.floor(tick / 10)
-    local turn_flag = (block % 2 == 0) and scubywasm.ACTION_TURN_RIGHT or scubywasm.ACTION_TURN_LEFT
-    return scubywasm.ACTION_THRUST | turn_flag | scubywasm.ACTION_FIRE
+    if rng_state[agent_id] == nil then
+        rng_state[agent_id] = agent_id & 0xffffffff
+    end
+
+
+    local action = 0
+    local s = rng_state[agent_id]
+    local r
+
+    s, r = next_rnd(s)
+    if r >= 0.9 then
+        is_moving[agent_id] = not is_moving[agent_id]
+    end
+    if is_moving[agent_id] then
+        action = action | scubywasm.ACTION_THRUST
+    end
+
+    s, r = next_rnd(s)
+    if r < 0.25 then
+        action = action | scubywasm.ACTION_TURN_LEFT
+    elseif r < 0.50 then
+        action = action | scubywasm.ACTION_TURN_RIGHT
+    end
+
+    s, r = next_rnd(s)
+    if r < 0.5 then
+        action = action | scubywasm.ACTION_FIRE
+    end
+
+    rng_state[agent_id] = s
+
+    return action
 end
